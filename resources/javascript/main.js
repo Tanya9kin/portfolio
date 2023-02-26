9a84444badd75cc0094c9bc5ae6b198ee33fe0b6
Tanya D <tanya9kin@gmail.com>
2023-02-26 19:26:44 +0200

    added navigation bar functionality

---
+++ resources/javascript/main.js
@@ -0,0 +1,13 @@
+// const dropdown = document.getElementsByClassName("dropdown")[0];
+// const dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+
+// const animateDropdownOut = (event) => {
+//   dropdownContent.style.animation = "rollIn 1s ease-in reverse forwards";
+// };
+
+// const animateDropdownIn = (event) => {
+//   dropdownContent.style.animation = "rollIn 1s ease-in";
+// };
+
+// dropdown.addEventListener("mouseenter", animateDropdownIn);
+// dropdown.addEventListener("mouseleave", animateDropdownOut);
